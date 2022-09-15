2aa996d6d9762855f61c58a623bc0eca4878acd6
jamiekim0223 <jamiekim0223@gmail.com>
2022-09-15 09:23:01 -0500

    Finished Lab

---
+++ src/Variables.java
@@ -0,0 +1,28 @@
+public class Variables
+{
+    public static void main ( String[] args )
+    {
+        //define 1 variable of each of the
+        //following data types
+        //byte	short		int 		long
+        //float	double
+        //char      boolean	String
+
+        //integer variables
+        byte byteOne = 127;
+        int intOne =123;
+        float floatOne =6745F;
+        String stringOne="Hello Everyone!";
+
+        System.out.println(""+intOne+" + "+stringOne+"");
+        System.out.println(floatOne);
+        System.out.println(byteOne);
+
+        System.out.println("/////////////////////////////////");
+        System.out.println("*Some Person            07/18/22*");
+        System.out.println("*                               *");
+        System.out.println("*        integer types          *");
+        System.out.println("*                               *");
+        System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+    }
+}
